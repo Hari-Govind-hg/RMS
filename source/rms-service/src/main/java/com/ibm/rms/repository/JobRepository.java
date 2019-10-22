0ccf34d0223c811816644975fc9b6b1e3518b7e9
ankit5678 <panigrahyankit1997@gmail.com>
2019-10-22 17:04:42 +0530

    clean old backend

--- source/rms-service/src/main/java/com/ibm/rms/repository/JobRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ibm.rms.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.ibm.rms.model.*;
-
-public interface JobRepository extends MongoRepository<Job, Object>{
-
-}
--- source/rms-service/src/main/java/com/ibm/rms/service/IJobService.java
+++
@@ -1,5 +0,0 @@
-package com.ibm.rms.service;
-
-public interface IJobService {
-
-}
